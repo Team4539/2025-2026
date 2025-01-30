d565288dc91db1cee183860197f7de0f02d887c5
Agame7k <comaidenaiden@gmail.com>
2025-01-29 22:12:29 -0600

    prepped code for positioning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,12 +11,31 @@ public class Constants {
         public static final int ElevatorEncoderID = 0; // Placeholder value
         public static final double ElevatorMaxHeight = 0; // Placeholder value
         public static final double ElevatorMinHeight = 0; // Placeholder value
+
+        /*Elevator heights */
+        public static final double HomePosition = 0; // Placeholder value
+        public static final double coralL1Position = 0; // Placeholder value
+        public static final double coralL2Position = 0; // Placeholder value
+        public static final double coralL3Position = 0; // Placeholder value
+        public static final double coralL4Position = 0; // Placeholder value
+        public static final double alageL1Position = 0; // Placeholder value
+        public static final double alageL2Position = 0; // Placeholder value
     }
     public static final class HeadRotator {
         public static final int HeadRotatorMotorID = 0; // Placeholder value
         public static final int HeadRotatorEncoderID = 0; // Placeholder value
         public static final double HeadRotatorMaxAngle = 0; // Placeholder value
         public static final double HeadRotatorMinAngle = 0; // Placeholder value
+
+        /*Rotation positions*/
+        public static final double HomeRotation = 0; // Placeholder value
+        public static final double algaeGroundRotation = 0; // Placeholder value
+        public static final double algaeProcessorsRotation = 0; // Placeholder value
+        public static final double algaeReefRotation = 0; // Placeholder value
+        public static final double algaeBargeRotation = 0; // Placeholder value
+        public static final double coralReefAngledRotation = 0; // Placeholder value
+        public static final double coralL4Rotation = 0; // Placeholder value
+
     }
     public static final class HeadMechanisms {
         public static final int CoralManipulatorMotorID = 0; // Placeholder value
